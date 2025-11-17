e87ee9c5274906dc3d3857d0ff61bf2772a062d2
meack <meack73@gmail.com>
2025-11-17 00:21:44 -0500

    making sure all files commit properly

--- backend/userAuth/controllers/authController.js
+++ backend/userAuth/controllers/authController.js
@@ -1,1 +1,72 @@
 
+const bcrypt = require("bycryptjs");
+const jwt = require("jsonwebtoken");
+const userRepo = require("../userAuth/repos/userRepo.memory"); //change when get DB
+
+const JWT_SECRET = "supersecret";       //change later
+const JWT_EXPIRES = "10m";
+const REFRESH_EXPIRES = "2d";
+
+function signAccessToken(user) {
+    return jwt.sign(
+        {
+            sub: user.id, email: user.email
+        },
+        JWT_SECRET,
+        {
+            expiresIN: JWT_EXPIRES
+        }
+    );
+}
+
+exports.register = async (request, response) => {
+    const { name = "", email, password } = request.body;
+
+
+    if (!email || !password) {
+        return response.status(400).json({ error: "Email and password is required" });
+    }
+
+    const exists = await userRepo.findByEmail(email);
+    if (exists) {
+        return response.status(409).json({ error: "This email is already attatched to another user." });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await userRepo.create({
+        name,
+        email,
+        hashPassword
+    });
+
+    const accessToken = signAccessToken(user);
+
+    response.status(201).json({
+        user,
+        accessToken
+    });
+
+};
+
+exports.login = async (request, response) => {
+    const { email, password } = request.body;
+
+    const user = await userRepo.findByEmail(email);
+    if (!user) {
+        return response.status(401).json({ error: "Invalid username or password. Please try again." });
+    }
+
+    const valid = await.bcrypt.compare(password, user.hashPassword);
+    if (!valid) {
+        return response.status(401).json({ error: "Invalid username or password. Please try again" });
+    }
+
+    const accessTojen = signAccessToken(user);
+
+    response.json({ user, accessToken });
+};
+
+exports.me = async (request, response) => {
+    response.json({ user: request.user });
+};
--- backend/userAuth/middleware/requireAuth.js
+++ backend/userAuth/middleware/requireAuth.js
@@ -1,1 +1,30 @@
+const jwt = require("jsonwebtoken");
+const userRepo = require("../userAuth/middleware/requireAuth"); //change when get DB
 
+const JWT_SECRET = "supersecret"; //change later
+
+module.exports = async (request, response, next) => {
+    const header = request.headers.authorization || "";
+    const token = header.startsWith("Bearer ") ? header.slice(7) : null;
+
+    if (!token) {
+        return response.status(401).json({ error: "Missing token" });
+    }
+
+    try {
+        const payload = jwt.verify(token, JWT_SECRET);
+
+        const user = await userRepo.findById(payload.sub);
+        if (!user) {
+            return response.status(401).json({ error: "User not found" });
+        }
+
+        request.user = user;
+        next();
+
+    }
+    catch (e) {
+        return response.status(401).json({ error: "Invalid token" });
+    }
+
+};
